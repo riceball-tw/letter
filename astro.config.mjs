cc49e04224cc96bf928dbcf87a8a55f33d158285
riceball <42161586+riceball-tw@users.noreply.github.com>
2026-04-12 23:09:39 +0800

    chore: Make env work in astro config base on doc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import { languages, prefixDefaultLocale, defaultLocale } from './src/i18n/i18n.ts';
+
+// ENV In the Astro config file
+// https://docs.astro.build/en/guides/environment-variables/#in-the-astro-config-file
+const { BASE_URL } = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +17,7 @@ export default defineConfig({
     '/work/': `/${defaultLocale}/work/`,
     '/faq/': `/${defaultLocale}/faq/`,
   },
-  ...(process.env.BASE_URL ? { base: process.env.BASE_URL } : {}),
+  ...(BASE_URL ? { base: BASE_URL } : {}),
   prefetch: {
     prefetchAll: true,
   },
